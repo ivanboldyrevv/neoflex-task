347eb8eff9d89b7c629408dc217c843f81fed19f
ivan <1ibah1ivan1@gmail.com>
2025-04-18 16:25:15 +0400

    money format and refactoring

--- src/main/java/neoflex/task/vacation_calculator/service/VacationService.java
+++ src/main/java/neoflex/task/vacation_calculator/service/VacationService.java
@@ -9,10 +9,14 @@ import neoflex.task.vacation_calculator.util.WorkdayUtil;
 import java.time.LocalDate;
 import java.util.Set;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class VacationService {
-    Set<LocalDate> productionCalendar;
+
+    private static final BigDecimal DAYS_IN_MONTH = BigDecimal.valueOf(29.3);
+    private final Set<LocalDate> productionCalendar;
 
     @Autowired
     public VacationService(HolidayProvider holidayProvider) {
@@ -20,12 +24,29 @@ public class VacationService {
     }
 
     public double calculate(double averageSalary, int vacationDays) {
-        return (averageSalary / 29.3) * vacationDays;
+        BigDecimal roundedAverageSalary = roundToScale(averageSalary);
+        BigDecimal vacationDaysBigDecimal = BigDecimal.valueOf(vacationDays);
+        
+        return calculateVacationSalary(roundedAverageSalary, vacationDaysBigDecimal);
     }
 
     public double calculate(double averageSalary, int vacationDays, LocalDate startDate) {
-        int workdays = (int) WorkdayUtil.countWorkdays(startDate, vacationDays, productionCalendar);
-        return (averageSalary / 29.3) * workdays;
+        BigDecimal roundedAverageSalary = roundToScale(averageSalary);
+        BigDecimal workdaysBigDecimal = BigDecimal.valueOf(
+            WorkdayUtil.countWorkdays(startDate, vacationDays, productionCalendar));
+        
+        return calculateVacationSalary(roundedAverageSalary, workdaysBigDecimal);
     }
 
+    private static BigDecimal roundToScale(double salary) {
+        return BigDecimal.valueOf(salary)
+                         .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private double calculateVacationSalary(BigDecimal salary, BigDecimal vacationDays) {
+        return salary.divide(DAYS_IN_MONTH, 4, RoundingMode.HALF_UP)
+                     .multiply(vacationDays)
+                     .setScale(2, RoundingMode.HALF_UP)
+                     .doubleValue();
+    }
 }
